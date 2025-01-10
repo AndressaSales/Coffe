6c57e2d2c9685cdc79316116a74cfd64eb1a30e6
AndressaSales <andressaccs2001@gmail.com>
2025-01-10 13:50:06 -0300

    finalizando a navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,16 @@
-import React, { useState } from 'react'
-import { IoIosMenu } from "react-icons/io";
+import React from 'react'
 const Navbar = () => {
-    
-    const Menu = [
-        {
-            id: 1,
-            name: "Home",
-        },
-        {
-            id: 2,
-            name: "Serviços"
-        },
-        {
-            id: 3,
-            name: "Sobre"
-        }
-    ]
 
   return (
-    <div className='abosute top-0 left-0 w-full pt-10 text-[#fff] z-20 bg-dark'>
+    <nav className='abosute top-0 left-0 w-full pt-10 text-[#fff] z-20 bg-dark'>
         <div className='container'>
             <div className='flex justify-between items-center'>
                 <a href="#" className='text-2xl font-semibold'>
                     <h1><span className='text-primary'>Dev</span> Coffe </h1>
                 </a>
-                
-                <div>
-                    <IoIosMenu className='text-3xl cursor-pointer' />
-                </div>
             </div>
         </div>
-    </div>
+    </nav>
   )
 }
 
